7558ef8e3bed7f708da8e43b87210884d51943ea
Maruf Hasan <marufhasanmitul@gmail.com>
2023-07-07 22:44:18 +0600

    Initial commit

---
+++ bin/practice13.dart
@@ -0,0 +1,93 @@
+void main() {
+  //List
+
+  //Map
+
+  //set
+
+  //Fixed Length List
+
+  //what is data
+  //String
+
+
+
+  //Growable List
+
+  //Access Item Of List
+
+  List<String> myList=["Rofiq","Shofiq","Jobbar"];
+  myList[0]="Rasel";
+
+  print(myList[0]);
+
+
+  //Get Index By Value
+
+
+  //Find The Length Of The List
+
+  //Changing Values Of List
+
+
+  //Some Method-add(),addAll(),insert(),insertAll()
+
+
+  //Insert Item To List
+
+
+
+
+  // && Logical and two value is true
+  // || Logical Or one value is true
+  // !=  Not equal
+  //== Equal
+
+  // >= greater than equal
+  // <= less than Equal
+
+
+  var a= 10;
+  var b= 20;
+
+  var c = ( a >= b ) ;
+
+  if(a > b && a<b){
+    print("i am True");
+  } else{
+    print("i am false");
+  }
+
+  print(c);
+
+
+  var x=90;
+  var y =80;
+
+  if(x==y){
+    print("I am Print");
+  }else if ( x<=y ){
+    print("I am Maruf");
+  } else{
+    print("I am else");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
---
+++ test/practice13_test.dart
@@ -0,0 +1,8 @@
+import 'package:practice13/practice13.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
