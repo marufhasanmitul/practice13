d00279874f65756d440c912be28c367deb3b9f1d
Maruf Hasan <marufhasanmitul@gmail.com>
2023-07-26 22:56:42 +0600

    function

--- bin/practice13.dart
+++ bin/practice13.dart
@@ -1,93 +1,22 @@
-void main() {
-  //List
+void main(){
+  //function //what is function //why use function//syntax
+  //Loop--for,foreach,while,doWhile,
 
-  //Map
-
-  //set
-
-  //Fixed Length List
-
-  //what is data
-  //String
-
-
-
-  //Growable List
-
-  //Access Item Of List
-
-  List<String> myList=["Rofiq","Shofiq","Jobbar"];
-  myList[0]="Rasel";
-
-  print(myList[0]);
-
-
-  //Get Index By Value
-
-
-  //Find The Length Of The List
-
-  //Changing Values Of List
-
-
-  //Some Method-add(),addAll(),insert(),insertAll()
-
-
-  //Insert Item To List
-
-
-
-
-  // && Logical and two value is true
-  // || Logical Or one value is true
-  // !=  Not equal
-  //== Equal
-
-  // >= greater than equal
-  // <= less than Equal
-
-
-  var a= 10;
-  var b= 20;
-
-  var c = ( a >= b ) ;
-
-  if(a > b && a<b){
-    print("i am True");
-  } else{
-    print("i am false");
-  }
-
-  print(c);
-
-
-  var x=90;
-  var y =80;
-
-  if(x==y){
-    print("I am Print");
-  }else if ( x<=y ){
-    print("I am Maruf");
-  } else{
-    print("I am else");
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(blockFunction(10,20));
+noParametarize();
+print(secondFunction(20 ,3));
 
 
 }
+//
+int blockFunction(int a,int b) => a+b;
 
+int secondFunction(int a,int b) {
+  return a*b;
+}
 
+void noParametarize(){
+  print("No Parametarize function");
+}
 
 
