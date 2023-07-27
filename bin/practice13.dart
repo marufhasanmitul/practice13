ad63b261cce1ea5573cf629f65b7b8e0364f58f8
Maruf Hasan <marufhasanmitul@gmail.com>
2023-07-27 21:19:01 +0600

    function

--- bin/practice13.dart
+++ bin/practice13.dart
@@ -1,22 +1,75 @@
+var a = 20;//Global Scope
+
 void main(){
-  //function //what is function //why use function//syntax
+  //function //what is function //why use function//syntax//Scope
   //Loop--for,foreach,while,doWhile,
 
-print(blockFunction(10,20));
-noParametarize();
-print(secondFunction(20 ,3));
+       var num =10;
+       var result=10%2;
+       print(result);
+
+       noParametarize();
+       //=============Map=========
+       Map<String,String> djkfhj= {
+         "sdfsa": "sadfsad",
+       };
+
+       List<int>  footballplayers=[1,2,3,4];//List
+       //foreach Loop
+       for(int player in  footballplayers){
+         print(player);
+       }
+
+
+
+
+
+
+
+
+
 
 
 }
-//
-int blockFunction(int a,int b) => a+b;
+//arrow function
+int arrowFunction()=>a;
 
+//Parametarize function
 int secondFunction(int a,int b) {
   return a*b;
 }
-
+//noParametarize
 void noParametarize(){
-  print("No Parametarize function");
+  var a=1;
+  do{
+    print(a);
+    a++;
+
+  }while(a<20);
+/*
+  for(int i=1;i<10;i++){
+    print(i);
+  }
+* */
+
+  //even Number
+  for(int i=1;i<10;i++){
+    if(i%2 == 0){
+      print(i);
+    }
+  }
+
+  //Odd Number
+  for(int i=1;i<10;i++){
+    if(i%2 == 0){
+      print(i);
+    }
+  }
+
+
+
+
+
 }
 
 
